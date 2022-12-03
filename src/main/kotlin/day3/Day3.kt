e620be19eabf8ade50b74bb10ffc069bed32b487
Henrik Berglund <henrik.berglund@leovegas.com>
2022-12-03 13:12:48 +0100

    Day3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,38 @@
+package day3
+
+import readInput
+
+val alphabet = ('a'..'z').joinToString(separator = "") { "$it" }
+val packItems = (alphabet + alphabet.uppercase()).mapIndexed { index, c ->
+    Pair(c, index + 1)
+}.toMap()
+
+val input = readInput("day3.txt")
+    .map {
+        Pair(it.subSequence(0, it.length / 2).toSet(),
+            it.subSequence(it.length / 2, it.length).toSet())
+    }
+
+fun solveA(): Long {
+    return input.map {
+        it.first.intersect(it.second).first()
+    }.mapNotNull { packItems[it] }.sum().toLong()
+}
+
+fun solveB(): Long {
+    return input.asSequence()
+        .chunked(3)
+        .map {
+            it.map { it.first.union(it.second) }
+        }
+        .map {
+            (it[0] intersect it[1] intersect it[2]).first()
+        }
+        .mapNotNull { packItems[it] }
+        .sum().toLong()
+}
+
+fun main() {
+    println("Answer A: ${solveA()}")
+    println("Answer B: ${solveB()}")
+}
